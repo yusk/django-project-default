0645a5ecfceb49e5fc78510a282f07e483bbe7b9
yusk <tdfagamdb@yahoo.co.jp>
2018-08-17 00:04:07 +0900

    clean: views

---
+++ main/views/__init__.py
@@ -0,0 +1,2 @@
+from .api import *
+from .html import *
--- main/views/api.py
+++ main/views/api.py
@@ -1,48 +1,12 @@
-from django.urls import reverse_lazy
-from django.conf import settings
-from django.views.generic import FormView, TemplateView
-from django.contrib.auth import login
-
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
 from rest_framework_jwt.settings import api_settings
 
 from main.models import User
-from main.forms import SignupForm, SigninForm
 from main.serializers import NoneSerializer, TokenOutputSerializer, UserSerializer
 
 jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
 jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
-
-
-class IndexView(TemplateView):
-    template_name = "main/index.html"
-
-
-class SignupView(FormView):
-    template_name = 'main/signup.html'
-    form_class = SignupForm
-    success_url = reverse_lazy('main:index')
-
-    def form_valid(self, form):
-        user = form.create_user()
-        if user is None:
-            return super().form_invalid(form)
-        login(self.request, user)
-        return super().form_valid(form)
-
-
-class SigninView(FormView):
-    template_name = 'main/signin.html'
-    form_class = SigninForm
-    success_url = reverse_lazy('main:index')
-
-    def form_valid(self, form):
-        user = form.get_authenticated_user()
-        if user is None:
-            return super().form_invalid(form)
-        login(self.request, user)
-        return super().form_valid(form)
 
 
 class RegisterDummyUserView(GenericAPIView):
@@ -67,4 +31,3 @@ class UserView(GenericAPIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
-
