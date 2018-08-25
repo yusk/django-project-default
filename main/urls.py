d6ff7c007ada7641e9310a14379ac7a29cc3259a
yusk <tdfagamdb@yahoo.co.jp>
2018-08-25 13:51:24 +0900

    feat: clean urls & add media url

--- main/urls.py
+++ main/urls.py
@@ -3,34 +3,34 @@ from django.conf import settings
 from django.contrib.auth.views import logout_then_login
 
 from rest_framework.routers import DefaultRouter, APIRootView
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 from . import views
 
+router = DefaultRouter()
+router.APIRootView = APIRootView
+router.register('users', views.UserViewSet, base_name='user')
+
 
 app_name = 'main'
-urlpatterns = []
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('api/', include(router.urls)),
+    path('api/auth/refresh/', refresh_jwt_token),
+    path('api/auth/verify/', verify_jwt_token),
+    path('api/user/', views.UserView.as_view()),
+    path('signup/', views.SignupView.as_view(), name='signup'),
+    path('signin/', views.SigninView.as_view(), name='signin'),
+    path('signout/', logout_then_login, name='signout'),
+]
 
 if settings.DEBUG:
     from rest_framework_swagger.views import get_swagger_view
-    from rest_framework.routers import DefaultRouter, APIRootView
-    from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
-
-    router = DefaultRouter()
-    router.APIRootView = APIRootView
-    router.register('users', views.UserViewSet, base_name='user')
 
     schema_view = get_swagger_view()
 
     urlpatterns.extend([
-        path('', views.IndexView.as_view(), name='index'),
-        path('api/', include(router.urls)),
         path('api/register/dummy/', views.RegisterDummyUserView.as_view()),
         path('api/auth/user/', obtain_jwt_token),
-        path('api/auth/refresh/', refresh_jwt_token),
-        path('api/auth/verify/', verify_jwt_token),
-        path('api/user/', views.UserView.as_view()),
-        path('signup/', views.SignupView.as_view(), name='signup'),
-        path('signin/', views.SigninView.as_view(), name='signin'),
-        path('signout/', logout_then_login, name='signout'),
         path('schema/', schema_view),
     ])
