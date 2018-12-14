87e335454a928d15d664dc1c0b3b35fa75116224
yusk <tdfagamdb@yahoo.co.jp>
2018-12-14 16:35:19 +0900

    close #13

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,9 +1,11 @@
+import os
+
 from .base import *
 
 SECRET_KEY = '76)5(c(fx4ton1h6ski0r1t3y9f$!q#q68+-drj3m&tmb9o8u_'
 DEBUG = True
 JWT_AUTH['JWT_SECRET_KEY'] = SECRET_KEY
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('ADDITIONAL_HOST')]
 
 DATABASES = {
     'default': {
