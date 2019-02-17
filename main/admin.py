2b27c765a8075f1dd3d862df7dd624b1ae0d66f5
yusk <tdfagamdb@yahoo.co.jp>
2019-02-17 13:57:47 +0800

    close #29

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from django.db.models import Model
 
-from main.models import User
+from main import models
 from main.utils import register_admin
 
-for cls in [User]:
-    register_admin(cls)
+for name in dir(models):
+    obj = getattr(models, name)
+    if not isinstance(obj, type):
+        continue
+
+    if issubclass(getattr(models, name), Model):
+        register_admin(obj)
