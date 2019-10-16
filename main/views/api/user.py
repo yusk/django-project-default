70b08ea8c1a662f0aee0c266c5076655e83ae399
yusk <tdfagamdb@yahoo.co.jp>
2019-10-16 15:38:20 +0900

    close #43

--- main/views/api/user.py
+++ main/views/api/user.py
@@ -4,10 +4,33 @@ from rest_framework.mixins import RetrieveModelMixin
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.decorators import schema
 from rest_framework.schemas import ManualSchema
-
+from django_filters import rest_framework as filters
 
 from main.models import User
 from main.serializers import UserSerializer
+
+
+class UserFilter(filters.FilterSet):
+    def get_by_tweet_info(self, queryset, name, value):
+        kwargs = {f"tweet__{name}": value}
+        return queryset.filter(**kwargs).distinct()
+
+    name__gt = filters.CharFilter(field_name='name', lookup_expr='gt')
+    name__lt = filters.CharFilter(field_name='name', lookup_expr='lt')
+    tweet = filters.CharFilter(method='get_by_tweet_info')
+
+    order_by = filters.OrderingFilter(
+        fields=(
+            ('id', 'id'),
+            ('name', 'name'),
+        ), )
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "name",
+        ]
 
 
 class UserView(GenericAPIView):
@@ -28,3 +51,6 @@ class UserView(GenericAPIView):
 class UserViewSet(RetrieveModelMixin, GenericViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    filter_class = UserFilter
+    ordering_fields = ('created_at', )
+    ordering = ('created_at', )
