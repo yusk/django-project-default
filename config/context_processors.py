82646790285dfb712237e15023005b344452debf
yusk <tdfagamdb@yahoo.co.jp>
2019-10-16 14:27:54 +0900

    close #40

--- config/context_processors.py
+++ config/context_processors.py
@@ -9,10 +9,10 @@ def ignore_google_analytics(request):
     return {"ignore_google_analytics": settings.IGNORE_GOOGLE_ANALYTICS}
 
 
-def is_logined(request):
-    return {"is_logined": request.user.is_authenticated}
+def is_logged_in(request):
+    return {"is_logged_in": request.user.is_authenticated}
 
 
-def logined_user(request):
+def logged_in_user(request):
     user = request.user if request.user.is_authenticated else None
-    return {"logined_user": user}
+    return {"logged_in_user": user}
