e3fa38965fb8041a353b31dd20e710786a97bd7a
yusk <tdfagamdb@yahoo.co.jp>
2019-10-16 14:31:09 +0900

    close #44

--- main/utils.py
+++ main/utils.py
@@ -1,10 +1,13 @@
 import uuid
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 import slackweb
+import numpy as np
+import pickle
 
 from django.db import models
 from django.contrib import admin
 from django.conf import settings
+from django.utils import timezone
 
 slack = slackweb.Slack(url=settings.SLACK_WEBHOOK_URL)
 
@@ -44,6 +47,8 @@ def json_serial(obj):
         return obj.isoformat()
     elif isinstance(obj, (uuid.UUID)):
         return str(obj)
+    elif isinstance(obj, np.ndarray):
+        return obj.tolist()
     raise TypeError("Type %s not serializable" % type(obj))
 
 
@@ -52,3 +57,59 @@ def slack_notify(text):
         slack.notify(text=text)
     except Exception as e:
         print(e)
+
+
+def get_next_minute_datetime(d):
+    return timezone.localtime(d + timedelta(minutes=1) - timedelta(
+        seconds=d.second) - timedelta(microseconds=d.microsecond))
+
+
+def get_prev_minute_datetime(d):
+    return timezone.localtime(d - timedelta(seconds=d.second) -
+                              timedelta(microseconds=d.microsecond))
+
+
+class MacOSFile(object):
+    def __init__(self, f):
+        self.f = f
+
+    def __getattr__(self, item):
+        return getattr(self.f, item)
+
+    def read(self, n):
+        # print("reading total_bytes=%s" % n, flush=True)
+        if n >= (1 << 31):
+            buffer = bytearray(n)
+            idx = 0
+            while idx < n:
+                batch_size = min(n - idx, 1 << 31 - 1)
+                # print("reading bytes [%s,%s)..." % (idx, idx + batch_size), end="", flush=True)
+                buffer[idx:idx + batch_size] = self.f.read(batch_size)
+                # print("done.", flush=True)
+                idx += batch_size
+            return buffer
+        return self.f.read(n)
+
+    def write(self, buffer):
+        n = len(buffer)
+        print("writing total_bytes=%s..." % n, flush=True)
+        idx = 0
+        while idx < n:
+            batch_size = min(n - idx, 1 << 31 - 1)
+            print(
+                "writing bytes [%s, %s)... " % (idx, idx + batch_size),
+                end="",
+                flush=True)
+            self.f.write(buffer[idx:idx + batch_size])
+            print("done.", flush=True)
+            idx += batch_size
+
+
+def pickle_dump(obj, file_path):
+    with open(file_path, "wb") as f:
+        return pickle.dump(obj, MacOSFile(f), protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def pickle_load(file_path):
+    with open(file_path, "rb") as f:
+        return pickle.load(MacOSFile(f))
