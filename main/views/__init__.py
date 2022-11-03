b51ce8ddc133be7c35edf5e9803e679d0963b31b
yusk <tdfagamdb@yahoo.co.jp>
2022-11-04 03:55:08 +0900

    clean view

--- main/views/__init__.py
+++ main/views/__init__.py
@@ -1,6 +1,2 @@
-from .auth import AuthUUIDView, AuthUserViewWithEmail  # noqa
-from .confirm import ConfirmDigitView, ConfirmDigitResetView  # noqa
-from .password import PasswordResetEmailView, PasswordResetDigitView, PasswordResetView  # noqa
-from .register import RegisterDummyUserView, RegisterUUIDView, RegisterUserView, RegisterUserViewWithEmail  # noqa
-from .status import StatusView  # noqa
+from .common import *  # noqa
 from .user import UserView, UserViewSet, UserPasswordView  # noqa
