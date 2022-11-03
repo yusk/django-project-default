df31625e5f1048e8912b373901024c2a01338515
yusk <tdfagamdb@yahoo.co.jp>
2022-11-04 00:29:09 +0900

    clean serializer

--- main/serializers/__init__.py
+++ main/serializers/__init__.py
@@ -1,9 +1,3 @@
-from .digit import DigitSerializer  # noqa
-from .email import EmailSerializer  # noqa
-from .message import MessageSerializer  # noqa
-from .name import NameSerializer  # noqa
-from .none import NoneSerializer  # noqa
+from .common import *  # noqa
 from .password_reset import PasswordResetSerializer  # noqa
-from .token import TokenSerializer  # noqa
 from .user import UserSerializer, UserSignUpSerializer, UserPasswordSerializer, UserDeleteSerializer  # noqa
-from .uuid import UUIDSerializer  # noqa
---
+++ main/serializers/common/__init__.py
@@ -0,0 +1,7 @@
+from .digit import DigitSerializer  # noqa
+from .email import EmailSerializer  # noqa
+from .message import MessageSerializer  # noqa
+from .name import NameSerializer  # noqa
+from .none import NoneSerializer  # noqa
+from .token import TokenSerializer  # noqa
+from .uuid import UUIDSerializer  # noqa
